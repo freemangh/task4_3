e258334020e9313f94db34c9d850a1e900bdb75d
root <khvastunov@gmail.com>
2018-04-01 02:03:43 +0300

    TMP task4_3.sh

--- task4_3.sh
+++ task4_3.sh
@@ -21,11 +21,11 @@ else
 		OUTFILE=$CURPATH/$SCRPATH'task4_3.out'
 		TMPFILE=$CURPATH/$SCRPATH'task4_3.tmp'
 		FILEMASK="$(echo $1 | cut -c 2- | tr '/' '-')"
-		FILENAME="$FILEMASK-$(date +%F_%H-%M-%S_%N)'.tar.gz'"
+		FILENAME="$FILEMASK-$(date +%F_%H-%M-%S_%N).tar.gz"
                 if [ ! -d "$BCKPDIR" ] ; then
                         /bin/mkdir -p $BCKPDIR
                 fi
-		/bin/tar -czvf "$BCKPDIR/$FILENAME" "$TARGET"
+		/bin/tar -czvf "$BCKPDIR/$FILENAME" "$TARGET" && ls -tp $BCKPDIR/$FILEMASK-*.tar.gz | grep -v '/$' | tail -n +$(($COUNT+1)) | xargs -I {} rm -- {}
 		#
 		else
 		echo "No such file or directory. Nothing to backup, you enter $1"
