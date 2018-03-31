38dbb849443b307d1abca2cf0a121e7d47264dd5
root <khvastunov@gmail.com>
2018-03-31 23:49:11 +0300

    TMP commit

--- task4_3.sh
+++ task4_3.sh
@@ -3,3 +3,7 @@
 # Task 4.3
 # Eugeniy Khvastunov
 # Simple back-up script
+CURPATH="$(pwd)"
+SCRPATH="$(echo $0 | rev | cut -c 11- | rev)"
+OUTFILE=$CURPATH/$SCRPATH'task4_3.out'
+TMPFILE=$CURPATH/$SCRPATH'task4_3.tmp'
